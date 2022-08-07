196b41e1e165560f4dcdf9aecfda027391cd6c90
mike09 <mike09@mail.ru>
2022-08-07 23:28:12 +0300

    add dependent for bulletins image

--- app/models/bulletin.rb
+++ app/models/bulletin.rb
@@ -8,5 +8,6 @@ class Bulletin < ApplicationRecord
 
   validates :title, presence: true, length: { minimum: 2 }
   validates :description, presence: true, length: { minimum: 15 }
-  validates :image, attached: true, size: { less_than: 5.megabytes }, content_type: %i[png jpg jpeg]
+  validates :image, attached: true, size: { less_than: 5.megabytes }, content_type: %i[png jpg jpeg],
+    dependent: :purge_later
 end
