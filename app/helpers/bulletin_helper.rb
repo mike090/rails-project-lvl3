70809aadf125c7614f803fdb512972bb38cc9736
mike09 <mike09@mail.ru>
2022-08-16 17:31:02 +0300

    lint

--- app/helpers/bulletin_helper.rb
+++ app/helpers/bulletin_helper.rb
@@ -1,14 +1,13 @@
 # frozen_string_literal: true
 
 module BulletinHelper
-
   BULLETIN_STATE_ICONS = {
-    draft:            'fa-solid fa-file-lines',
+    draft: 'fa-solid fa-file-lines',
     under_moderation: 'fa-solid fa-magnifying-glass',
-    published:        'fa-solid fa-circle-check',
-    rejected:         'fa-solid fa-reply',
-    archived:         'fa-solid fa-file-zipper'
-  }
+    published: 'fa-solid fa-circle-check',
+    rejected: 'fa-solid fa-reply',
+    archived: 'fa-solid fa-file-zipper'
+  }.freeze
 
   def draw_bulletin_state(bulletin)
     render 'web/shared/bulletin_state', icon_class: BULLETIN_STATE_ICONS[bulletin.state.to_sym], title: t(bulletin.state)
--- config/routes.rb
+++ config/routes.rb
@@ -2,11 +2,10 @@
 
 Rails.application.routes.draw do
   scope module: :web do
-
     namespace :admin do
       root 'categories#index'
       resources :categories, only: %i[index new create edit update destroy]
-      resources :bulletins , only: %i[index] do
+      resources :bulletins, only: %i[index] do
         collection do
           get 'moderate'
         end
@@ -32,6 +31,5 @@ Rails.application.routes.draw do
     post 'auth/:provider', to: 'auth#request', as: :auth_request
     get 'auth/:provider/callback', to: 'auth#callback', as: :callback_auth
     delete 'auth/sing_out', as: :sing_out
-
   end
 end
