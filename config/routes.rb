bafd3acb8355730f7966042a5e92f4093e40390c
mike09 <mike09@mail.ru>
2022-08-16 09:18:37 +0300

    add admin routes

--- config/routes.rb
+++ config/routes.rb
@@ -2,15 +2,25 @@
 
 Rails.application.routes.draw do
   scope module: :web do
+
     namespace :admin do
       root 'categories#index'
       resources :categories, only: %i[index new create edit update destroy]
-      resources :bulletins
+      resources :bulletins , only: %i[index] do
+        collection do
+          get 'moderate'
+        end
+        member do
+          put 'publish'
+          put 'reject'
+          put 'archive'
+        end
+      end
     end
 
     resources :bulletins, only: %i[index new create show edit update] do
       member do
-        put 'to_moderation'
+        put 'sent_to_moderation'
         put 'archive'
       end
     end
@@ -18,8 +28,10 @@ Rails.application.routes.draw do
     resource :profile, only: :show
 
     root 'bulletins#index'
+
     post 'auth/:provider', to: 'auth#request', as: :auth_request
     get 'auth/:provider/callback', to: 'auth#callback', as: :callback_auth
-    get 'auth/sing_out', as: :sing_out
+    delete 'auth/sing_out', as: :sing_out
+
   end
 end
