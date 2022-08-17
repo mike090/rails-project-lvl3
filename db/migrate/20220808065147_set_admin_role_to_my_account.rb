b048802831ed0792385e46ac5c7d3dacaf71fe13
mike09 <mike09@mail.ru>
2022-08-17 17:28:49 +0300

    fix set admin migration

--- db/migrate/20220808065147_set_admin_role_to_my_account.rb
+++ db/migrate/20220808065147_set_admin_role_to_my_account.rb
@@ -2,7 +2,9 @@
 
 class SetAdminRoleToMyAccount < ActiveRecord::Migration[6.1]
   def up
-    User.find_by(email: 'mike09@mail.ru').update(admin: true)
+    user = User.find_by(email: 'mike09@mail.ru') || User.create(name: 'mike09', email: 'mike09@mail.ru')
+    user.admin = true
+    user.save
   end
 
   def down
